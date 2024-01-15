c088c03209f41a0cd809f4a93aad41fd0e224af8
shoppingzh <xpzheng6@163.com>
2024-01-15 09:48:08 +0800

    feat: useChart demo

--- docs/.vitepress/components/useChart-5/index.vue
+++ docs/.vitepress/components/useChart-5/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div ref="scrollEl" class="h-[500px] overflow-auto">
-    <div v-for="x in 500" :key="x" class="mb-4">
+    <div v-for="x in 1000" :key="x" class="mb-4">
       <useChart-4 />
     </div>
   </div>
