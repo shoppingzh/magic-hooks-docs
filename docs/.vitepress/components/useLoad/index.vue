40122ecbb6b40683f6f7720b0e1c0c5a1f5906d9
shoppingzh <xpzheng6@163.com>
2024-05-28 17:17:52 +0800

    feat: useLoad doc

--- docs/.vitepress/components/useLoad/index.vue
+++ docs/.vitepress/components/useLoad/index.vue
@@ -14,7 +14,16 @@
     <el-table-column prop="title" label="标题" show-overflow-tooltip min-width="120"></el-table-column>
     <el-table-column prop="body" label="内容" show-overflow-tooltip min-width="200"></el-table-column>
   </el-table>
-  <div class="mt-4 text-gray-500 text-xs text-right">总数：{{ result.length }}</div>
+  <div class="mt-4 flex items-center">
+    <div class="flex-1 w-0">
+      加载完成状态：
+      <el-tag v-if="loaded" type="success">已完成</el-tag>
+      <el-tag v-else type="danger">未完成</el-tag>
+    </div>
+    <div class="text-gray-500 text-xs text-right">
+      总数：{{ result.length }}
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -23,7 +32,10 @@ import { ref, onMounted, readonly } from 'vue'
 
 // const all = ref([])
 const loading = ref(false)
-const { query, result, load } = useLoad(async() => {
+const { query, result, loaded, load } = useLoad(async() => {
+  await new Promise(r => {
+    setTimeout(r, 2000)
+  })
   const params = Object.entries(query).filter(([key, value]) => !!value).map(([key, value]) => `${key}=${value}`).join('&')
   const res = await fetch(`https://jsonplaceholder.typicode.com/posts?${params}`)
   const data: any[] = await res.json()
--- docs/api/useLoad.md
+++ docs/api/useLoad.md
@@ -10,6 +10,7 @@
 
 - 抽象的加载方法，不限定加载源
 - 提供查询条件与loading态
+- 加载完成状态
 
 ## 使用方法
 
@@ -46,18 +47,32 @@ load()
 **types**
 
 ```ts
-type LoadFn<Q, R> = (query: Q) => Promise<R>
+export type LoadFn<Q, R> = (query: Q) => Promise<R>
 
-interface UseLoadOptions<Q, R> {
+export interface UseLoadOptions<Q, R> {
+  /** 初始查询条件 */
   initialQuery?: MaybeRef<Q>
+  /** 默认加载结果 */
+  initialResult?: MaybeRef<R>
+  /** 加载中状态 */
+  loading?: MaybeRef<boolean>
+  /** 自动加载 */
+  autoLoad?: boolean
 }
 
-interface UseLoadReturn<Q, R> {
+export interface UseLoadReturn<Q, R> {
+  /** 查询条件 */
   query: Q
+  /** 结果 */
   result: Ref<R>
+  /** 加载中状态 */
   loading: Ref<boolean>
+  /** 加载完成状态(只读) */
+  loaded: ComputedRef<boolean>
+  /** 加载 */
   load: (disableLoading?: boolean) => Promise<void>
 }
+
 ```
 
 
